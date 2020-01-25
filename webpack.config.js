6f005c3281740e2fb72ebba9c66b155c1a001729
kswanson25 <katieswanson259@gmail.com>
2020-01-24 21:34:43 -0800

    Run Webpack Successfully

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+
+
+module.exports = {
+  entry: __dirname + '/src/index.js',
+  module: {
+    rules: [
+      {
+        test: [/\.m?(js|jsx)$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+   output: {
+    filename: 'bundle.js',
+    path: __dirname + '/client/dist'
+  }
+};
