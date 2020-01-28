4cb1d5579bd84d992636bca037bcec56ee3d9783
kswanson25 <katieswanson259@gmail.com>
2020-01-27 22:30:20 -0800

    Returns Correct Status After Req/Res is Complete

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3003; /*process.env.PORT || 3003*/
+const port = 3003;
 var db = require ('../database/index.js');
 const bodyParser = require('body-parser');
 
