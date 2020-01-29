c1eb3cb437af632d19ccc8e25c6c3f191708533b
kswanson25 <katieswanson259@gmail.com>
2020-01-29 00:01:20 -0800

    Render Experience Data To Front End
    Not formatted, dummy data.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Welcome(props) {
-  return <h3>Hello, {props.name}</h3>
+// function Welcome(props) {
+//   return <h3>Hello, {props.name}</h3>
+// }
+
+// const element = <Welcome name="Katie" />;
+
+// ReactDOM.render(element, document.getElementById('app'));
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3003/2')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json
+        })
+      });
+  }
+
+  render() {
+    var { isLoaded, items } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+          <ul>
+            {items.map(item => (
+              <li key={item.photo_id}>
+                user: {item.username}  |  photo id: {item.photo_id}
+                <img src={item.photo_url} />
+              </li>
+            ))};
+          </ul>
+
+      );
+    }
+  }
 }
 
-const element = <Welcome name="Katie" />;
-
-ReactDOM.render(element, document.getElementById('app'));
+  ReactDOM.render(<App/>, document.getElementById('app'));
