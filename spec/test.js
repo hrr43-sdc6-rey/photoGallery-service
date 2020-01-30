a3e6d5c9f23f17e06879df2a15e6b38a6da07656
kswanson25 <katieswanson259@gmail.com>
2020-01-30 02:21:56 -0800

    Front End Tests In Progress
    Currently first test is failing.

--- spec/test.js
+++
@@ -1,8 +0,0 @@
-//import React from 'react';
-// const renderItem = require('../src/index.js');
-
-const sum = require('../sum');
-
-test('adds 1 + 2 to equal 3', () => {
-  expect((1, 2)).toBe(3);
-});
